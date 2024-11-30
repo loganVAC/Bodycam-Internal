676c045d741ffc8364b4afb4902afa86d0fb3226
Admin <Admin@DESKTOP-LHHH3UC>
2024-11-29 23:04:12 -0500

    writing ESP :P

--- Bodycam Internal/ESP.h
+++ Bodycam Internal/ESP.h
@@ -8,3 +8,17 @@
 #include "globals.h"
 
 
+// Constants
+constexpr Vec2 ScreenCenter{ 2560 / 2, 1440 / 2 };
+constexpr float PI{ 3.14159265359f };
+
+namespace ESP {
+	// Helper Function for drawing shit cause how else we supposed to see the shit
+	inline void DrawWithOutline(std::function<void()> drawFunc, float outlineThickness, ImColor outlineColor)
+	{
+		if (outlineThickness > 0.f)
+		{
+			ImGui::PushStyleVar(ImGuiStyleVar_Alpha, 0.5f); //this is optional im pretty sure but if u dont do this u cant see the line?
+		}
+	}
+}
